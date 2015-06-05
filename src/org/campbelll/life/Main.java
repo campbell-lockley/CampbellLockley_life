59dd2f30cef3d4f89438433c0f8ab289092c9390
Campbell Lockley <campbell.lockley@gmail.com>
2015-06-05 16:39:36 +1200

    Adapted Main.java for Amazon Cloud runs.

--- src/org/campbelll/life/Main.java
+++ src/org/campbelll/life/Main.java
@@ -13,44 +13,93 @@ import java.util.concurrent.TimeoutException;
  */
 public class Main {
 	/* Run parameters */
-	final static int GENERATIONS = 4080;
-//	final static int GENERATIONS = 40800;
-	final static int BOARD_SIZE = 1024;
-//	final static int BOARD_SIZE = 4096;
+//	final static int GENERATIONS = 4080;	// My parameters
+//	final static int BOARD_SIZE = 1024;
+//	final static int NUM_THREADS = 4;
+//	final static int CACHE_SIZE = 32;
+	final static int GENERATIONS = 20;		// Amazon parameters
+	final static int BOARD_SIZE = 32768;
+	final static int NUM_THREADS = 32;
+	final static int CACHE_SIZE = 32;
 
 	/**
 	 * Entry point when running the jar.
 	 * 
 	 * @param args Command line arguments.
+	 */
+	public static void main(String[] args)  {
+		Life life;
+
+		/* Run all LineParallelLife runs */
+		System.out.println("LineParallelLife: Starting " + GENERATIONS + 
+				" generations of " + BOARD_SIZE + "x" + BOARD_SIZE);
+		for (int i = 1; i <= NUM_THREADS; i++) {
+			System.out.print(i + " Thread: ");
+			life = new LineParallelLife(BOARD_SIZE, i);
+			try {
+				runTest(life);
+			} catch (IOException | FileFormatException e) {
+				System.err.println("LineParallelLife run "+i+" failed.");
+			}
+		}
+		
+		/* Run all BlockParallelLife runs */
+		System.out.println("BlockParallelLife: Starting " + GENERATIONS + 
+				" generations of " + BOARD_SIZE + "x" + BOARD_SIZE);
+		for (int i = 1; i <= NUM_THREADS; i++) {
+			System.out.print(i + " Thread: ");
+			life = new BlockParallelLife(BOARD_SIZE, i);
+			try {
+				runTest(life);
+			} catch (IOException | FileFormatException e) {
+				System.err.println("BlockParallelLife run "+i+" failed.");
+			}
+		}
+
+		/* Run SequentialLife run */
+		System.out.println("SequentialLife: Starting " + GENERATIONS + 
+				" generations of " + BOARD_SIZE + "x" + BOARD_SIZE);
+		life = new SequentialLife(BOARD_SIZE);
+		try {
+			runTest(life);
+		} catch (IOException | FileFormatException e) {
+			System.err.println("SequentialLife run failed.");
+		}
+		
+		/* Run all CacheParallelLife runs */
+		System.out.println("CacheParallelLife: Starting " + GENERATIONS + 
+				" generations of " + BOARD_SIZE + "x" + BOARD_SIZE);
+		for (int i = 1; i <= NUM_THREADS; i++) {
+			System.out.print(i + " Thread: ");
+			life = new CacheParallelLife(BOARD_SIZE, i, CACHE_SIZE);
+			try {
+				runTest(life);
+			} catch (IOException | FileFormatException e) {
+				System.err.println("CacheParallelLife run "+i+" failed.");
+			}
+		}
+	}
+	
+	/**
+	 * Sets up and runs a given implementation of {@link Life}.
+	 * 
+	 * @param life The Game of Life implementation to run.
 	 * @throws IOException if there is an I/O error.
 	 * @throws FileFormatException if the pattern file is formated incorrectly.
 	 */
-	public static void main(String[] args) 
+	public static void runTest(Life life) 
 			throws IOException, FileFormatException {
-		final int numThreads = Runtime.getRuntime().availableProcessors();
-		final int cacheSize = 32;		// 32k L1 cache for my machine
-//		marku_life.SequentialLife life = 
-//				new marku_life.SequentialLife(BOARD_SIZE);
-//		Life life = new SequentialLife(BOARD_SIZE);
-//		Life life = new LineParallelLife(BOARD_SIZE, numThreads);
-		Life life = new BlockParallelLife(BOARD_SIZE, numThreads);
-//		Life life = new CacheParallelLife(BOARD_SIZE, numThreads, cacheSize);
-		
 		InputStream input = Main.class
 				.getResourceAsStream("/gosperGliderGun.patt");
 		life.loadPattern(input);
 		
-		System.out.print("Warming up ...");
 		try {
 			life.warmup(100);
 		} catch (TimeoutException e) {
 			System.err.println("warmup() timed out");
 			e.printStackTrace();
 		}
-		System.out.println(" Done");
 		
-		System.out.println("Starting " + GENERATIONS + " generations of "
-				+ BOARD_SIZE + "x" + BOARD_SIZE);
 		final long startTime = System.nanoTime();
 		for (int gen = 0; gen < GENERATIONS; gen++) {
 			try {
@@ -63,7 +112,6 @@ public class Main {
 		final long endTime = System.nanoTime();
 		System.out.println("Time taken was " + (endTime - startTime) / 1.0e9 + 
 				" secs");
-		life.printBoard(40, 40);
 		life.cleanUp();
 	}
 	
